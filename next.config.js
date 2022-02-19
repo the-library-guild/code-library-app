4a3aedbd2da111a4a6e7592c13603d23bed957a0
Linus Bolls <linusbolls@Linus-MacBuch-Luft.local>
2022-02-19 19:58:21 +0100

    added ne sache

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    GRAPHQL_URL: "http://localhost:4000/graphql/",
-    CLIENT_URL: "http://localhost:3000/",
+    GRAPHQL_URL: isProd
+      ? "https://code-library-api.herokuapp.com/graphql/"
+      : "http://localhost:4000/graphql/",
+    CLIENT_URL: isProd
+      ? "https://code-library-client.herokuapp.com"
+      : "http://localhost:3000/",
   },
 };
 module.exports = nextConfig;
