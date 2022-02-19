47be55d17adcf6c0388840e1123ee862385097b3
Linus Bolls <linusbolls@Linus-MacBuch-Luft.local>
2022-02-19 21:01:22 +0100

    lkajwer

--- token.ts
+++ token.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest } from "next";
 import type { NextRequest } from "next/server";
 import type { JWT } from "next-auth/jwt";
+import { getToken } from "next-auth/jwt";
 import jwt from "jsonwebtoken";
 
 function signToken(tokenData: any) {
@@ -15,10 +16,11 @@ function verifyToken(tokenStr: unknown) {
     return null;
   }
 }
-function tokenFromRequest(req: NextRequest | NextApiRequest) {
+async function tokenFromRequest(req: NextRequest | NextApiRequest) {
   const { cookies } = req;
   const tokenStr = cookies["next-auth.session-token"];
+  console.log(tokenStr);
 
-  return verifyToken(tokenStr);
+  return await getToken({ req: req as any, secret: process.env.JWT_SECRET });
 }
 export { signToken, verifyToken, tokenFromRequest };
