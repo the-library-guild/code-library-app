41531d8eacb8afc546daf1f9de4587802a52ae2e
Linus Bolls <linusbolls@Linus-MacBuch-Luft.local>
2022-02-21 19:23:16 +0100

    feat: changed library logo

--- components/svgs/LibraryLogo.tsx
+++ components/svgs/LibraryLogo.tsx
@@ -1,36 +1,42 @@
 function LibraryLogo({ ...rest }) {
   return (
-    <svg
-      viewBox="0 0 46 21"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...rest}
-    >
-      <path
-        d="M32 20H26V2.75L32 6.30556V20Z"
-        stroke="#222222"
-        strokeWidth="2"
-        fill="none"
-        strokeLinecap="square"
+    <svg viewBox="0 0 138 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="77.4773"
+        y="1.73648"
+        width="10"
+        height="10"
+        transform="rotate(-10 77.4773 1.73648)"
+        fill="#222222"
       />
-      <path
-        d="M14 20V2"
-        stroke="#222222"
-        strokeWidth="2"
-        strokeLinecap="square"
+      <rect
+        x="80.082"
+        y="16.5086"
+        width="10"
+        height="46"
+        transform="rotate(-10 80.082 16.5086)"
+        fill="#222222"
       />
-      <path
-        d="M20 20V2"
-        stroke="#222222"
-        strokeWidth="2"
-        strokeLinecap="square"
+      <rect
+        x="88.9381"
+        y="66.7338"
+        width="10"
+        height="10"
+        transform="rotate(-10 88.9381 66.7338)"
+        fill="#222222"
       />
+      <rect x="64" y="0.290939" width="10" height="10" fill="#222222" />
+      <rect x="64" y="15.2909" width="10" height="46" fill="#222222" />
+      <rect x="64" y="66.2909" width="10" height="10" fill="#222222" />
+      <rect x="44" y="0.290939" width="10" height="10" fill="#222222" />
+      <rect x="44" y="15.2909" width="10" height="46" fill="#222222" />
+      <rect x="44" y="66.2909" width="10" height="10" fill="#222222" />
       <path
-        d="M10 6.5L7.5 4L0 11.5L7.5 19L10 16.5L5 11.5L10 6.5Z"
+        d="M33.7778 21.7354L25.3333 13.2909L0 38.6243L25.3333 63.9576L33.7778 55.5132L16.8889 38.6243L33.7778 21.7354Z"
         fill="#222222"
       />
       <path
-        d="M36 16.5L38.5 19L46 11.5L38.5 4L36 6.5L41 11.5L36 16.5Z"
+        d="M104 55.5132L112.444 63.9576L137.778 38.6243L112.444 13.2909L104 21.7354L120.889 38.6243L104 55.5132Z"
         fill="#222222"
       />
     </svg>
