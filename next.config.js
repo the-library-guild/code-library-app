ff34144ec0d00df6de5954ac054868d820ec57ce
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-03-21 19:34:04 +0100

    Scream when sensitive env variables are not set

--- next.config.js
+++ next.config.js
@@ -5,15 +5,28 @@ const capitalizedEnvMode = envMode.charAt(0).toUpperCase() + envMode.slice(1);
 
 const log = (arg) => console.info(`[Client] ${arg}`);
 
-log(`Available on ${process.env.NEXT_PUBLIC_CLIENT_URL} in ${capitalizedEnvMode} Mode`);
-log(`GraphQl Url: ${process.env.NEXT_PUBLIC_GRAPHQL_URL}`);
-log(`Auth Url: ${process.env.NEXTAUTH_URL}`);
+const getEnvVarOrDie = value => {
+  if (process.env[value] == null) {
+    throw new Error(
+      `Environment variable ${value} is not set`
+    )
+  }
+  return process.env[value];
+}
+
+const clientUrl = getEnvVarOrDie('NEXT_PUBLIC_CLIENT_URL');
+const graphqlUrl = getEnvVarOrDie('NEXT_PUBLIC_GRAPHQL_URL');
+const nextauthUrl = getEnvVarOrDie('NEXTAUTH_URL');
+
+log(`Available on ${clientUrl} in ${capitalizedEnvMode} Mode`);
+log(`GraphQl Url: ${graphqlUrl}`);
+log(`Auth Url: ${nextauthUrl}`);
 
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-    NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
+    NEXT_PUBLIC_GRAPHQL_URL: graphqlUrl,
+    NEXT_PUBLIC_CLIENT_URL: clientUrl,
   },
   experimental: {
     outputStandalone: true,
