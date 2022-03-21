8842d12f3190ec7825e20e1eb03ad811e497367c
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-03-21 21:42:18 +0100

    Pass backend API url as build arg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
+    NEXT_PUBLIC_GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL
+  },
   experimental: {
     outputStandalone: true,
   },
