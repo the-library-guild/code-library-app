7c9f37cfa86a7bfb9959820762caf4a45c7ad362
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-03-21 19:41:06 +0100

    Assert all env variables are set before building app

--- next.config.js
+++ next.config.js
@@ -1,18 +1,35 @@
 /** @type {import('next').NextConfig} */
 
+const { assert } = require('console');
+
 const envMode = process.env.NODE_ENV || "development";
 const capitalizedEnvMode = envMode.charAt(0).toUpperCase() + envMode.slice(1);
 
 const log = (arg) => console.info(`[Client] ${arg}`);
 
-const getEnvVarOrDie = value => {
+const assertVariableIsSet = value => {
   if (process.env[value] == null) {
     throw new Error(
       `Environment variable ${value} is not set`
-    )
+    );
   }
+}
+
+const getEnvVarOrDie = value => {
+  assertVariableIsSet(value);
+
   return process.env[value];
 }
+
+const requiredVariables = [
+ 'MAX_SESSION_DURATION_SECONDS',
+ 'JWT_SECRET',
+ 'NEXTAUTH_SECRET',
+ 'GOOGLE_ID',
+ 'GOOGLE_SECRET',
+];
+
+requiredVariables.map(value => assertVariableIsSet(value));
 
 const clientUrl = getEnvVarOrDie('NEXT_PUBLIC_CLIENT_URL');
 const graphqlUrl = getEnvVarOrDie('NEXT_PUBLIC_GRAPHQL_URL');
