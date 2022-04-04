f85c6bfa337ea8c7b3f5292bd84f1fb7829ad805
Linus Bolls <linus.bolls@code.berlin>
2022-04-04 21:19:40 +0200

    refactor: merged BookCard and pages/books/[id] to use the same base component

--- src/components/Booklist/BookCard.tsx
+++ src/components/Booklist/BookCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 import {
   Box,
+  Flex,
+  Spacer,
+  Button,
   useColorModeValue,
   Stat,
   StatLabel,
@@ -10,27 +14,21 @@ import {
 } from "@chakra-ui/react";
 
 import { Book } from ".";
-import Link from "next/link";
+import { useBookState } from "../../hooks/use-book-state.hook";
 
-interface CardProps {
+const programAcronym = (contentTags: string[]) => contentTags[3];
+
+interface BookCardProps {
   book: Book;
+  isExpanded?: boolean;
 }
+export function BookCard({ book, isExpanded = false }: BookCardProps) {
+  const { data: session } = useSession();
 
-export function BookCard({ book }: CardProps) {
-  const {
-    rentable: { stateTags },
-  } = book;
-
-  const bookIsAvailable = stateTags.includes("Available");
-
-  return bookIsAvailable ? (
-    <AvailableBookCard book={book} />
-  ) : (
-    <NonAvailableBookCard book={book} />
+  const { label, color, hasAction, actionLabel, action } = useBookState(
+    book,
+    session
   );
-}
-
-const NonAvailableBookCard = ({ book }: CardProps) => {
   const { name, media } = book;
 
   return (
@@ -41,37 +39,68 @@ const NonAvailableBookCard = ({ book }: CardProps) => {
       p={3}
     >
       <Stat>
-        <StatLabel>PM</StatLabel>
+        <StatLabel>
+          {media?.contentTags ? programAcronym(media?.contentTags) : "Unknown"}
+        </StatLabel>
         <StatNumber>
-          <Link href={`/books/${book._id}`}>{name}</Link>
+          <Link href={`/books/${book?._id}`}>{name}</Link>
         </StatNumber>
         <StatHelpText>{media?.tagline}</StatHelpText>
-        <StatHelpText color={useColorModeValue("red.800", "red.300")}>
-          Not Available
-        </StatHelpText>
+        <StatHelpText color={color}>{label}</StatHelpText>
       </Stat>
+      {isExpanded && (
+        <Flex>
+          <Spacer />
+          {hasAction && (
+            <Button marginLeft={"auto"} onClick={() => action && action()}>
+              {actionLabel}
+            </Button>
+          )}
+        </Flex>
+      )}
     </Box>
   );
-};
+}
+// const NonAvailableBookCard = ({ book }: BookCardProps) => {
+//   return (
+//     <Box
+//       rounded={"lg"}
+//       bg={useColorModeValue("white", "gray.700")}
+//       boxShadow={"lg"}
+//       p={3}
+//     >
+//       <Stat>
+//         <StatLabel>PM</StatLabel>
+//         <StatNumber>
+//           <Link href={`/books/${book._id}`}>{name}</Link>
+//         </StatNumber>
+//         <StatHelpText>{media?.tagline}</StatHelpText>
+//         <StatHelpText color={useColorModeValue("red.800", "red.300")}>
+//           Not Available
+//         </StatHelpText>
+//       </Stat>
+//     </Box>
+//   );
+// };
 
-const AvailableBookCard = ({ book }: CardProps) => {
-  const { name, media } = book;
+// const AvailableBookCard = ({ book }: BookCardProps) => {
+//   const { name, media } = book;
 
-  return (
-    <Box
-      rounded={"lg"}
-      bg={useColorModeValue("white", "gray.700")}
-      boxShadow={"lg"}
-      p={3}
-    >
-      <Stat>
-        <StatLabel>SE</StatLabel>
-        <StatNumber>
-          <Link href={`/books/${book._id}`}>{name}</Link>
-        </StatNumber>
-        <StatHelpText>{media?.tagline}</StatHelpText>
-        <StatHelpText color={"primary.100"}>Available</StatHelpText>
-      </Stat>
-    </Box>
-  );
-};
+//   return (
+//     <Box
+//       rounded={"lg"}
+//       bg={useColorModeValue("white", "gray.700")}
+//       boxShadow={"lg"}
+//       p={3}
+//     >
+//       <Stat>
+//         <StatLabel>SE</StatLabel>
+//         <StatNumber>
+//           <Link href={`/books/${book._id}`}>{name}</Link>
+//         </StatNumber>
+//         <StatHelpText>{media?.tagline}</StatHelpText>
+//         <StatHelpText color={"primary.100"}>Available</StatHelpText>
+//       </Stat>
+//     </Box>
+//   );
+// };
--- src/pages/books/[id].tsx
+++ src/pages/books/[id].tsx
@@ -1,30 +1,13 @@
 import React from "react";
-import Link from "next/link";
-
-import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { Stack } from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
 
 import { Perm } from "code-library-perms";
 
-import {
-  Box,
-  Button,
-  Stack,
-  Stat,
-  StatHelpText,
-  StatLabel,
-  StatNumber,
-  useColorModeValue,
-  Flex,
-  Spacer,
-} from "@chakra-ui/react";
-
-import { gql, useQuery } from "@apollo/client";
-
 import { Content } from "../../layout/Content";
 import { InternalPage } from "../../layout/InternalPage";
-import { useRouter } from "next/router";
 import { FullPageSpinner } from "../../components/FullPageSpinner";
-import { useBookState } from "../../hooks/use-book-state.hook";
 import { BookCard } from "../../components/Booklist/BookCard";
 
 const GET_BOOK = gql`
@@ -47,12 +30,8 @@ const GET_BOOK = gql`
     }
   }
 `;
-const programAcronym = (contentTags: string[]) => contentTags[3];
-
 function BookDetailedPage() {
   const { query } = useRouter();
-
-  const { data: session } = useSession();
 
   const bookId = query.id as string;
 
@@ -61,68 +40,22 @@ function BookDetailedPage() {
   });
   const book = data?.getBook;
 
-  const { label, hasAction, actionLabel, action } = useBookState(book, session);
-
   if (loading) return <FullPageSpinner />;
-
-  if (error || !book) return <h1>Could not find requested book</h1>;
-
-  const {
-    name,
-    media,
-    rentable: { stateTags },
-  } = book;
-
-  const bookIsAvailable = stateTags.includes("Available");
 
   return (
     <InternalPage>
       <Content px={4}>
         <Stack w={"100%"} spacing={4} mt={4}>
-          <Box
-            rounded={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={3}
-          >
-            <Stat>
-              <StatLabel>
-                {media?.contentTags
-                  ? programAcronym(media?.contentTags)
-                  : "Unkown"}
-              </StatLabel>
-              <StatNumber>
-                <Link href={`/books/${book._id}`}>{name}</Link>
-              </StatNumber>
-              <StatHelpText>{media?.tagline}</StatHelpText>
-              {bookIsAvailable ? (
-                <StatHelpText
-                  color={useColorModeValue("green.800", "green.300")}
-                >
-                  Available
-                </StatHelpText>
-              ) : (
-                <StatHelpText color={useColorModeValue("red.800", "red.300")}>
-                  Not Available
-                </StatHelpText>
-              )}
-            </Stat>
-
-            <Flex>
-              <Spacer />
-              {hasAction && (
-                <Button marginLeft={"auto"} onClick={() => action && action()}>
-                  {actionLabel}
-                </Button>
-              )}
-            </Flex>
-          </Box>
+          {error ? (
+            <h1>Could not find requested book</h1>
+          ) : (
+            <BookCard book={book} isExpanded={true} />
+          )}
         </Stack>
       </Content>
     </InternalPage>
   );
 }
-
 BookDetailedPage.permissions = Perm.VIEW_BOOKS;
 
 export default BookDetailedPage;
