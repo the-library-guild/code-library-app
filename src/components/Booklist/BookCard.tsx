a6ca65f34e62e8a61166dac93bde9e56b8491ce8
Linus Bolls <linus.bolls@code.berlin>
2022-04-04 18:48:10 +0200

    WIP-2: Book detailed page

--- src/components/Booklist/BookCard.tsx
+++ src/components/Booklist/BookCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
   Box,
@@ -64,7 +64,7 @@ const AvailableBookCard = ({ book }: CardProps) => {
       <Stat>
         <StatLabel>SE</StatLabel>
         <StatNumber>{name}</StatNumber>
-        <StatHelpText>{media.tagline}</StatHelpText>
+        <StatHelpText>{media?.tagline}</StatHelpText>
         <StatHelpText color={"primary.100"}>Available</StatHelpText>
       </Stat>
     </Box>
--- src/pages/books/[id].tsx
+++ src/pages/books/[id].tsx
@@ -26,46 +26,28 @@ const GET_BOOK = (bookId: string) => {
         }
       }
   }
-  `
-}
+  `;
+};
 
 function BookDetailedPage() {
   const { query } = useRouter();
-  const bookId = query.id as string;
 
-  console.log(`
-  query GetBook {
-    getBook(bookId: "${bookId}") {
-      _id
-      name
-      tags
-      rentable {
-        dueDate
-        stateTags
-        rentedDate
-      }
-    }
-}
-`);
+  const bookId = query.id as string;
 
   const { loading, error, data } = useQuery(GET_BOOK(bookId));
 
   if (loading) return <FullPageSpinner />;
 
-  if (error) return <h1>Leave me alone!</h1>;
-
-  console.log(data);
-
   const book = data?.getBook;
 
-  if (!book) return <h1>Weird...</h1>
+  if (error || !book) return <h1>Could not find requested book</h1>;
 
   return (
     <InternalPage>
       <Content px={4}>
         <Stack align={"center"}>
           <Heading fontSize={"4xl"}>{query.id}</Heading>
-          <BookCard book={book}/>
+          <BookCard book={book} />
         </Stack>
       </Content>
     </InternalPage>
