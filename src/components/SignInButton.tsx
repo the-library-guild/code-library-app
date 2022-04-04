912f44d79d65d496318d9e1f4ab3c46998184872
Linus Bolls <linus.bolls@code.berlin>
2022-04-04 17:41:39 +0200

    fix: set login button width to 100%

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 
 import { ButtonHTMLAttributes } from "react";
 
 import { signIn } from "next-auth/react";
 
-import { FaGoogle } from 'react-icons/fa';
+import { FaGoogle } from "react-icons/fa";
 
 import { Button, Icon } from "@chakra-ui/react";
 
 type SignInButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function SignInButton({ ...rest }: SignInButtonProps) {
-
   return (
     <Button
-      onClick={() => signIn('google')}
+      onClick={() => signIn("google")}
       bg={"primary.100"}
       color={"gray.700"}
+      width={"100%"}
       px="8"
       _hover={{
-        bg: "primary.200"
+        bg: "primary.200",
       }}
       {...rest}
     >
       <Icon as={FaGoogle} marginRight={2} />
       Sign in with @code.berlin
-  </Button>
+    </Button>
   );
 }
