23007891c64bed4b936d2326badddd9e4905751c
Linus Bolls <linus.bolls@code.berlin>
2022-04-04 17:52:03 +0200

    fix: shrink booklist desktop view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent } from "react";
 
 import { InternalPage } from "../layout/InternalPage";
 
 import { Booklist } from "../components/Booklist/Booklist";
 
 import { Perm } from "code-library-perms";
-import { Content } from '../layout/Content';
-import { Heading, Input, Stack } from '@chakra-ui/react';
-import { FullPageSpinner } from '../components/FullPageSpinner';
-import { useSearch } from '../components/Search/use-search.hook';
+import { Content } from "../layout/Content";
+import { Heading, Input, Stack } from "@chakra-ui/react";
+import { FullPageSpinner } from "../components/FullPageSpinner";
+import { useSearch } from "../components/Search/use-search.hook";
 
 function IndexPage() {
   const { loading, error, books, searchTerm, setSearchTerm } = useSearch();
@@ -17,13 +17,13 @@ function IndexPage() {
 
   return (
     <InternalPage>
-      <Content px={4}>
-        <Stack spacing={6} py={8} px={8} wordBreak={"break-all"} w={'100%'}>
+      <Content px={4} justify={"center"}>
+        <Stack spacing={6} py={8} px={8} wordBreak={"break-all"} w={"70%"}>
           <SearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <Stack align={"center"}>
             <Heading fontSize={"4xl"}>All Books</Heading>
           </Stack>
-            {loading ? <FullPageSpinner /> : <Booklist books={books} />}
+          {loading ? <FullPageSpinner /> : <Booklist books={books} />}
         </Stack>
       </Content>
     </InternalPage>
@@ -45,8 +45,12 @@ function SearchBox({ searchTerm, setSearchTerm }: SearchBoxProps) {
   }
 
   return (
-    <Input type={'string'} placeholder={'Search for books'} size={'md'} value={searchTerm} onChange={handleChange} />
+    <Input
+      type={"string"}
+      placeholder={"Search for books"}
+      size={"md"}
+      value={searchTerm}
+      onChange={handleChange}
+    />
   );
 }
-
-
