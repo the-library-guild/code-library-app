373f86a4be2f37571bf04a4b94922bceca0d1555
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-04-11 20:26:36 +0200

    Use HttpLink to configure credentials proxying

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -1,9 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const uri = process.env.NEXT_PUBLIC_GRAPHQL_URL;
 
 export const apiClient = new ApolloClient({
-  uri,
+  link: new HttpLink({
+    uri,
+    credentials: 'include',
+  }),
   cache: new InMemoryCache(),
-  credentials: 'include',
 });
