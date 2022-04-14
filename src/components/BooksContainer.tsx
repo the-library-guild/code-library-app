8fb10f08c23d0a837ad304dea36d7771c9290d49
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-04-15 01:22:38 +0200

    Pull slicing logic out of presentational container

--- src/components/BooksContainer.tsx
+++ src/components/BooksContainer.tsx
@@ -8,19 +8,30 @@ interface BooksProps {
   loading: boolean;
   error: any;
   books: Book[];
-  maxIdx: number;
 }
 
-export function BooksContainer({ loading, error, books, maxIdx }: BooksProps) {
+export const BooksContainer = React.memo(function BooksContainer({
+  loading,
+  error,
+  books,
+}: BooksProps) {
   if (loading) return <FullPageSpinner />;
-  if (error) return <div>{`Error! ${error.message}`}</div>;
-  if (!books?.length) return <div>Could not find any books :(</div>;
+  if (error) return <Error error={error} />;
+  if (books?.length === 0) return <EmptyShelf />;
 
   return (
     <>
-      {books.slice(0, maxIdx).map((book) => (
+      {books.map((book) => (
         <BookCard key={book?._id} book={book} />
       ))}
     </>
   );
+});
+
+function Error({ error }: { error: Error }) {
+  return <div>{`Error! ${error.message}`}</div>;
+}
+
+function EmptyShelf() {
+  return <div>We could not find any books :(</div>;
 }
