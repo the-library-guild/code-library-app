f4bc98a2485d5e9f0860be500c79aeb7da3ba5b4
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-04-25 21:46:22 +0200

    Add debugging for env variable

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -6,6 +6,8 @@ const config = getConfig();
 
 const uri = config.publicRuntimeConfig.NEXT_PUBLIC_GRAPHQL_URL as string;
 
+console.log('NEXT_PUBLIC_GRAPHQL_URL', uri);
+
 export const apiClient = new ApolloClient({
   link: new HttpLink({
     uri,
