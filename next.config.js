46646b86866a0d9e033e2f1ce3168adfd505486a
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-04-25 20:52:18 +0200

    Ensure public values are defined at build time

--- next.config.js
+++ next.config.js
@@ -1,19 +1,29 @@
 /** @type {import('next').NextConfig} */
 
+const graphqlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+if (!graphqlUrl) {
+  throw Error('Please defined NEXT_PUBLIC_GRAPHQL_URL.');
+}
+
+const clientUrl = process.env.NEXT_PUBLIC_CLIENT_URL;
+if (!clientUrl) {
+  throw Error('Please defined NEXT_PUBLIC_CLIENT_URL.');
+}
+
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
-    NEXT_PUBLIC_GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL
+    NEXT_PUBLIC_CLIENT_URL: clientUrl,
+    NEXT_PUBLIC_GRAPHQL_URL: graphqlUrl,
   },
   serverRuntimeConfig: {
     GRAPHQL_URL: process.env.GRAPHQL_URL,
-    IS_PROD: process.env.NODE_ENV === 'production'
+    IS_PROD: process.env.NODE_ENV === 'production',
   },
   experimental: {
     outputStandalone: true,
   },
   eslint: {
-    dirs: ['src']
-  }
+    dirs: ['src'],
+  },
 };
--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const uri = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+const clientUrl = process.env.NEXT_PUBLIC_CLIENT_URL;
+
+console.log('NEXT_PUBLIC_GRAPHQL_URL', uri);
+console.log('NEXT_PUBLIC_CLIENT_URL', clientUrl);
 
 export const apiClient = new ApolloClient({
   link: new HttpLink({
