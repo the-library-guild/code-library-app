9f772b682270ea3b2f2430dc295c969f235f98ae
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-04-27 19:01:15 +0200

    Inline SidebarContent markup for better composability

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,36 +1,77 @@
 import React, { ReactNode } from 'react';
+import NextLink from 'next/link';
+
 import {
-  Box,
-  useColorModeValue,
   Drawer,
   DrawerContent,
-  useDisclosure,
   DrawerOverlay,
+  Box,
+  Flex,
+  Link,
+  Text,
 } from '@chakra-ui/react';
-import { Header } from './Header';
-import { SidebarContent } from './SidebarContent';
 
-export function Sidebar({ children }: { children: ReactNode }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+import { LibraryLogo } from '../LibraryLogo';
+import { useColorModeVariant } from '../../hooks/use-color-mode-variant.hook';
+import { SHELF } from '../../routes';
+
+interface SidebarProps {
+  onClose: any;
+  isOpen: boolean;
+  children: ReactNode;
+}
+
+export function Sidebar({ onClose, isOpen, children }: SidebarProps) {
+  const lightOrDark = useColorModeVariant();
 
   return (
-    <Box minH="100vh" bg={useColorModeValue('white', 'gray.800')}>
-      <Drawer
-        autoFocus={false}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        closeOnOverlayClick
-        size={'xs'}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      <Header onOpen={onOpen} />
-      <Box>{children}</Box>
-    </Box>
+    <Drawer
+      autoFocus={false}
+      isOpen={isOpen}
+      placement="left"
+      onClose={onClose}
+      returnFocusOnClose={false}
+      closeOnOverlayClick
+      size={'xs'}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <Box
+          transition="3s ease"
+          bg={lightOrDark('white', 'gray.900')}
+          borderRight="1px"
+          borderRightColor={lightOrDark('gray.200', 'gray.700')}
+          w={'full'}
+          pos="fixed"
+          h="full"
+        >
+          <Flex
+            h="20"
+            alignItems="center"
+            mx="8"
+            justifyContent="space-between"
+          >
+            <NextLink href={SHELF} passHref>
+              <Link
+                style={{ textDecoration: 'none' }}
+                _focus={{ boxShadow: 'none' }}
+                onClick={onClose}
+              >
+                <Flex
+                  as={'h2'}
+                  fontWeight={'bold'}
+                  gap={'2'}
+                  alignItems={'center'}
+                >
+                  <LibraryLogo height={'24'} />
+                  <Text fontSize={'2xl'}>Treedom Library</Text>
+                </Flex>
+              </Link>
+            </NextLink>
+          </Flex>
+          {children}
+        </Box>
+      </DrawerContent>
+    </Drawer>
   );
 }
