1da1533226decdb4cab7476c9d410e5a179270ec
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-05-01 20:48:20 +0200

    Set content maxW in pixels

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,7 +8,7 @@ interface ContentProps extends FlexProps {
 
 export function Content({ children, ...props }: ContentProps) {
   return (
-    <Flex maxW={'50rem'} w={'100%'} p={2} py={4} {...props}>
+    <Flex maxW={1200} w={'100%'} p={4} {...props}>
       {children}
     </Flex>
   );
