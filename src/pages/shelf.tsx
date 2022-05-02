ab66d26a487c5bd35f5d6c6e33ff16737e5ae805
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-05-02 15:21:07 +0200

    Use ref to avoid unecessary re-renders

--- src/pages/shelf.tsx
+++ src/pages/shelf.tsx
@@ -1,33 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+
 import { Stack } from '@chakra-ui/react';
 
 import { Perm } from 'code-library-perms';
-
 import { Content } from '../components/Content';
 import { useSearch } from '../components/Search/use-search.hook';
 import { SearchBox } from '../components/Search/SearchBox';
-import { GET_SHELF } from '../queries/queries';
-import { useBookContainer } from '../components/BookCard/use-book-container.hook';
 import { BooksContainer } from '../components/BooksContainer';
 import { InfiniteScroll } from '../components/InfiniteScroll';
 import { ResultsCount } from '../components/ResultsCount';
+import { Book } from '../services/code-library-server/books';
+import { useShelf } from '../hooks/use-shelf.hook';
 function ShelfPage() {
-  const { loading, error, books } = useBookContainer(GET_SHELF);
-  const { results, setInitialResults, searchTerm, setSearchTerm } = useSearch();
+  const { results, ...search } = useSearch();
+  const { loading, error, books } = useShelf();
+
+  // Avoids rerenders with similar results
+  const lastBooks = useRef<Book[]>([]);
 
   useEffect(() => {
-    if (loading) return;
-    setInitialResults(books);
-  }, [loading]);
+    if (loading) {
+      return;
+    }
+
+    if (lastBooks.current.length === books.length) {
+      return;
+    }
+
+    lastBooks.current = books;
+
+    search.setInitialResults(books);
+  }, [books, loading, search]);
 
   return (
     <Content>
       <Stack spacing={4} width="100%">
-        <SearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <SearchBox
+          searchTerm={search.searchTerm}
+          setSearchTerm={search.setSearchTerm}
+        />
         {!loading && (
           <ResultsCount
             results={results}
-            text={searchTerm ? 'Results' : 'Books in the shelf'}
+            text={search.searchTerm ? 'Results' : 'Books in the shelf'}
           />
         )}
         <Stack spacing={6} wordBreak="break-all" width="100%">
