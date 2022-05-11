29027c004fd0e24366bb0a268bccbf17f42f2c42
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-05-11 19:57:51 +0200

    Hide app name in mobile
    This allows us to shove more feature in the header for quick access instead of hiding everything under a drawer.

--- src/components/Sidebar/HomeLink.tsx
+++ src/components/Sidebar/HomeLink.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import NextLink from 'next/link';
 
-import { Text, Link } from '@chakra-ui/react';
+import { Text, Link, useBreakpointValue } from '@chakra-ui/react';
 import { SHELF } from '../../helpers/routes';
 
 export function HomeLink() {
+  const isNotMobile = useBreakpointValue({ base: false, md: true });
   return (
     <NextLink href={SHELF} passHref>
       <Link style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-        <Text as={'h2'} fontWeight={'bold'} fontSize={'2xl'}>
-          Treedom Library
-        </Text>
+        {isNotMobile && (
+          <Text as={'h2'} fontWeight={'bold'} fontSize={'2xl'}>
+            Treedom Library
+          </Text>
+        )}
       </Link>
     </NextLink>
   );
