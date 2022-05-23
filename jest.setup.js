6f7c73bee71e72e2b37c02332c38594e54360fe8
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-05-23 18:22:38 +0200

    Use cross-fetch for better compatibility

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
 
-import { loadEnvConfig } from '@next/env'
+import fetch from 'cross-fetch';
 
-export default async () => {
-  const projectDir = process.cwd()
-  loadEnvConfig(projectDir)
-}
+window.fetch = fetch;
