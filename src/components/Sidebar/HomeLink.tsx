2fa211190edcd4ba3ac805bffa28bd7c59fff6eb
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-05-25 18:45:07 +0200

    Reduce font-size of homelink instead of hidding

--- src/components/Sidebar/HomeLink.tsx
+++ src/components/Sidebar/HomeLink.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import NextLink from 'next/link';
 
-import { Text, Link, useBreakpointValue } from '@chakra-ui/react';
+import { Text, Link } from '@chakra-ui/react';
 import { SHELF } from '../../helpers/routes';
 
 export function HomeLink() {
-  const isNotMobile = useBreakpointValue({ base: false, md: true });
   return (
     <NextLink href={SHELF} passHref>
       <Link style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-        {isNotMobile && (
-          <Text as={'h2'} fontWeight={'bold'} fontSize={'2xl'}>
-            Treedom Library
-          </Text>
-        )}
+        <Text as={'h2'} fontWeight={'bold'} fontSize={['xl', '2xl']}>
+          Treedom Library
+        </Text>
       </Link>
     </NextLink>
   );
