3ad574e0f6bf521dea93fdd4181db2502b53c6de
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-06-09 22:07:06 +0200

    Remove chakra-ui storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,18 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  "stories": [
+  stories: [
     "../src/components/**/*.stories.mdx",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@chakra-ui/storybook-addon",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
   },
   webpackFinal: async (config) => {
     config.resolve.plugins = [
