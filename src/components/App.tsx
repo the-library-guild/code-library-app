24be7ebb79530f664a8250006c7b838460e090a8
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-06-20 14:29:43 +0200

    Extract app providers into standalone component

---
+++ src/components/App.tsx
@@ -0,0 +1,61 @@
+import { NextPage } from 'next';
+
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+import { AppProps as NextAppProps } from 'next/app';
+
+import { InternalPage } from './InternalPage';
+import { PageAuthorizer } from './PageAuthorizer';
+import { PermissionDeniedWidget } from './PermissionDeniedWidget';
+import { MakingAppContextProvider } from 'making-app-context';
+import { theme } from '@/helpers/theme';
+
+export type NextPageWithLayout = NextPage & {
+  permissions: number;
+  title: string;
+};
+
+export type LibraryAppProps = NextAppProps & {
+  Component: NextPageWithLayout;
+};
+
+export type AppProps = {
+  Component: NextPageWithLayout;
+  pageProps: any;
+};
+
+export function LibraryApp({ Component, pageProps }: AppProps) {
+  return Component.permissions ? (
+    <PageAuthorizer requiredPermissions={Component.permissions}>
+      {({ user, hasRequiredPermissions }) => {
+        return (
+          <InternalPage user={user} title={Component.title}>
+            {hasRequiredPermissions ? (
+              <Component {...pageProps} />
+            ) : (
+              <PermissionDeniedWidget>
+                <Button as={'a'} href={'/shelf'} w={'100%'}>
+                  Explore our shelf
+                </Button>
+              </PermissionDeniedWidget>
+            )}
+          </InternalPage>
+        );
+      }}
+    </PageAuthorizer>
+  ) : (
+    <Component {...pageProps} />
+  );
+}
+
+export function LibraryAppWithProviders({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <MakingAppContextProvider>
+        <LibraryApp Component={Component} pageProps={pageProps} />
+      </MakingAppContextProvider>
+    </ChakraProvider>
+  );
+}
+
+export default LibraryAppWithProviders;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,32 +2,15 @@ import React from 'react';
 
 import Head from 'next/head';
 
-import type { AppProps } from 'next/app';
-
-import { ChakraProvider, Stack, Text } from '@chakra-ui/react';
-
 import { SessionProvider } from 'next-auth/react';
 
 import { ApolloProvider } from '@apollo/client';
 
-import { CodeLibraryServer } from '../services/code-library-server';
+import { LibraryAppProps, LibraryAppWithProviders } from '@/components/App';
 
-import { PageAuthorizer } from '../components/PageAuthorizer';
-import { NextPage } from 'next';
-import { InternalPage } from '../components/InternalPage';
-import { MakingAppContextProvider } from '../making-app-context';
-import { theme } from '@/helpers/theme';
+import { CodeLibraryServer } from '@/services/code-library-server';
 
-type NextPageWithLayout = NextPage & {
-  permissions: number;
-  title: string;
-};
-
-type CustomAppProps = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function App({ Component, pageProps }: CustomAppProps) {
+function App({ Component, pageProps }: LibraryAppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={0}>
       <ApolloProvider client={CodeLibraryServer}>
@@ -38,40 +21,9 @@ function App({ Component, pageProps }: CustomAppProps) {
             content="CODE University of Applied Sciences Library Management System"
           />
         </Head>
-        <ChakraProvider theme={theme}>
-          <MakingAppContextProvider>
-            <LibraryApp Component={Component} pageProps={pageProps} />
-          </MakingAppContextProvider>
-        </ChakraProvider>
+        <LibraryAppWithProviders Component={Component} pageProps={pageProps} />
       </ApolloProvider>
     </SessionProvider>
-  );
-}
-
-type LibraryAppProps = {
-  Component: NextPageWithLayout;
-  pageProps: any;
-};
-
-function LibraryApp({ Component, pageProps }: LibraryAppProps) {
-  return Component.permissions ? (
-    <PageAuthorizer requiredPermissions={Component.permissions}>
-      {({ user, hasRequiredPermissions }) => {
-        return (
-          <InternalPage user={user} title={Component.title}>
-            {hasRequiredPermissions ? (
-              <Component {...pageProps} />
-            ) : (
-              <Stack spacing={4} width="100%">
-                <Text>Unfortunately you do not access this feature :(</Text>
-              </Stack>
-            )}
-          </InternalPage>
-        );
-      }}
-    </PageAuthorizer>
-  ) : (
-    <Component {...pageProps} />
   );
 }
 
