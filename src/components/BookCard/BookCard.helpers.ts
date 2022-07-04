257bce9cb97b6c19c3d70321d6272b3298706aff
Marcelo T. dos Santos <marcelo.teixeira@code.berlin>
2022-07-04 14:53:04 +0200

    Update due date to up to three weeks

--- src/components/BookCard/BookCard.helpers.ts
+++ src/components/BookCard/BookCard.helpers.ts
@@ -2,10 +2,10 @@ export const programAcronym = (contentTags?: string[] | null) =>
   contentTags ? contentTags[3] : 'Unknown';
 
 export const dueDateFromNow = () => {
-  const twoWeeksInMs = 1000 * 60 * 60 * 24 * 14;
+  const threeWeeksInMs = 1000 * 60 * 60 * 24 * 21;
   const now = new Date();
   const rentedDate = now.getTime();
-  const dueDate = new Date(rentedDate + twoWeeksInMs);
+  const dueDate = new Date(rentedDate + threeWeeksInMs);
 
   return [rentedDate, dueDate];
 };
